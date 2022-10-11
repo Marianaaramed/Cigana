704f9c339a30139bf05d7e70018faa1f942a4238
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2022-10-10 22:00:32 -0300

    Create sobre.c

---
+++ sobre.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sobre.h"
+
+
+
+/////
+// Funções do Módulo Sobre
+
+
+
+void telaSobre(void) {
+    sistema("clear || cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                    Universidade Federal do Rio Grande do Norte                    ///\n");
+    printf("///                    Centro de Ensino Superior do Seridó                            ///\n");
+    printf("///                    Departamento de Computação e Tecnologia                        ///\n");
+    printf("///                    Disciplina DCT1106 - Programação                               ///\n");
+    printf("///                    Projeto Cigana Virtual                                         ///\n");
+    printf("///                    Desenvolvido por mariana - 2022.2                              ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                      = = = = = = Cigana Virtual  = = = = = =                      ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///          Programa  utilizado na disciplina DCT1106 - Programação, para fins       ///\n");
+    printf("///          avaliativo no Semestre 2021.2. O programa contém os principais módulos   ///\n");
+    printf("///          e funcionalidades que serão exigidos ao longo da disciplina.             ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+
+
+void telaEquipe(void) {
+    sistema("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                   Universidade Federal do Rio Grande do Norte                     ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                             ///\n");
+    printf("///                   Departamento de Computação e Tecnologia                         ///\n");
+    printf("///                   Disciplina DCT1106 - Programação                                ///\n");
+    printf("///                   Projeto Cigana Virtual                                          ///\n");
+    printf("///                   Desenvolvido por mariana - 2022.2                               ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                      = = = = = = Cigana Virtual = = = = = =                       ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///             Este projeto exemplo foi desenvolvido por:                            ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///             Mariana Araújo de Medeiros                                            ///\n");
+    printf("///             E-mail: mariana.medeiros.017@ufrn.edu.br                              ///\n");
+    printf("///             Repositório: https://github.com/Marianaaramed/Cigana.git              ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
