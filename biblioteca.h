98f0db7bd05822fe6ce34b7aa25352cf6ab87a42
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2022-10-17 20:45:47 -0300

    Create biblioteca.h

---
+++ biblioteca.h
@@ -0,0 +1,14 @@
+void delay(int);
+void limpaTela(void);
+int ehDigito(char); 
+int ehDigito(char);
+int ehLetra(char);
+int ehBissexto(int);
+int ehData(int, int, int);
+
+int validarNome(char*);
+int validarCPF(char*);
+int validarEmail(char*);
+int validarData(char*);
+int validarCelular(char*);
+int validarNum(char*)
