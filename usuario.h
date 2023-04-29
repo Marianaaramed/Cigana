1ff1b6589ea6170165c61667e66cf439a9a1bfa0
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2023-04-29 15:23:04 -0300

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -13,30 +13,15 @@
 //////
 ////// Assisnaturas das funcões Módulo Usuario
 //////
-typedef struct usuario Usuario;
-
-struct usuario {
-    char CPF[12];
-    char nome[51];
-    char email[51];
-    char nasc[11];
-    char celular[12];
-    int status;
-};
 
 void moduloUsuario(void);
 char menuUsuario(void);
-Usuario* telaCadastrUsuario(void);
-char* telaPesquisarUsuario(void);
+void telaCadastrarUsuario(void);
+void telaPesquisarUsuario(void);
 void telaAlterarUsuario(void);
 void telaExcluirUsuario(void);
-void gravarUsuario(Usuario*)
 
 void cadastrarUsuario(void);
 void pesquisarUsuario(void);
 void alterarUsuario(void);
 void excluirUsuario(void);
-
-Usuario* buscarUsuario(char);
-void exibirUsuario(Usuario*);
-void regravarUsuario(Usuario*);
