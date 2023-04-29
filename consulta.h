02295f35ea9721623012d9d1db75fd6592b1fbe9
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2023-04-29 20:08:30 -0300

    Create consulta.h

---
+++ consulta.h
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                   ///
+///                   Universidade Federal do Rio Grande do Norte                     ///
+///                   Centro de Ensino Superior do Seridó                             ///
+///                   Departamento de Computação e Tecnologia                         ///
+///                   Disciplina DCT1106 - Programação                                ///
+///                   Projeto Cigana Virtual                                          ///
+///                   Desenvolvido por mariana                                        ///
+///                                                                                   ///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////
+////// Assisnaturas das funcões Módulo Consulta
+//////
+
+void moduloConsulta(void);
+char menuConsulta(void);
+void telaCadastrarConsulta(void);
+void telaPesquisarConsulta(void);
+void telaAlterarConsulta(void);
+void telaExcluirConsulta(void);
+
+void cadastrarConsulta(void);
+void pesquisarConsulta(void);
+void alterarConsulta(void);
+void excluirConsulta(void);
