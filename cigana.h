698b1248bd7a72e7391cdc0d01bfd14b3ba3c12c
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2023-05-22 08:47:14 -0300

    Create cigana.h

---
+++ cigana.h
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+///                                                                                   ///
+///                   Universidade Federal do Rio Grande do Norte                     ///
+///                   Centro de Ensino Superior do Seridó                             ///
+///                   Departamento de Computação e Tecnologia                         ///
+///                   Disciplina DCT1106 - Programação                                ///
+///                   Projeto Cigana Virtual                                          ///
+///                   Desenvolvido por mariana                                        ///
+///                                                                                   ///
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////
+// Assinatura das funções
+
+void telaPrincipal(void);
+void telaSobre(void);
+void telaEquipe(void);
