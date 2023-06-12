53a1d4bc8e7c0006ea0dcc1c967d4e3a06547722
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2023-06-12 15:52:40 -0300

    Update relatorio.h

--- relatorio.h
+++ relatorio.h
@@ -1,12 +1,12 @@
 void moduloRelatorio(void);
 char menuRelatorio(void);
-void tempoPorconsultar(void);
-void usuariosPorconsultor(void);
-void oraculosPorconsultor(void);
-char* telaTempoPorconsultar(void);
-char* telaUsuariosPorconsultor(void);
-char* telaOraculosPorconsultor(void);
-void relatTempoPorconsultar(char*);
-void relatUsuariosPorconsultor(char*);
-void relatOraculosPorconsultor(char*);
-void listaOraculosPorconsultor(char*);
+char*listrar_usuario(void);
+char*listrar_assessor(void);
+char* listrar_consulta(void);
+
+char* telaListrar_usuario(void);
+char* telaListrar_assessor(void);
+char* telaListrar_consulta(void);
+void relatListrar_usuario(char*);
+void relatListrar_assessor(char*);
+void relatListrar_consulta(char*);
