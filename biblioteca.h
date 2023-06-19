cfef32533e443dd70f3c5412a14874baa5dcbdc9
Marianaaramed <112339563+Marianaaramed@users.noreply.github.com>
2023-06-19 09:23:27 -0300

    Update biblioteca.h

--- biblioteca.h
+++ biblioteca.h
@@ -3,14 +3,17 @@
 
 void delay(int);
 void limpaTela(void);
-int Digito(char);
 int letra(char);
+int Digito(char);
 int ehBissexto(int);
+int Data(int, int, int);
+int valida_id(char, int);
+int caracteres_email(char);
 
 int validar_nome(char*);
 int validar_cpf(char*);
 int validar_email(char*);
-int valida_data(char*);
+int validar_data(char*);
 int validar_hora(char*);
 int validar_ramo(char*);
 int validar_codConsulta(char*);
